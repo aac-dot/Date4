875450021b0cdb3c788f4ddd7171bf6a1f4aa8ac
aac-dot <ayrton.andrade1@gmail.com>
2024-11-08 21:13:04 -0300

    feat: Add directory creation in FileSystem

--- src/main/java/com/tutego/date4u/core/FileSystem.java
+++ src/main/java/com/tutego/date4u/core/FileSystem.java
@@ -1,10 +1,24 @@
 package com.tutego.date4u.core;
 
+import java.io.IOException;
+import java.io.UncheckedIOException;
+import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+
+import org.springframework.boot.autoconfigure.ssl.SslProperties.Bundles.Watch.File;
 
 public class FileSystem {
     private final Path root = 
         Paths.get(System.getProperty("user.home")).resolve("fs");
-    
+
+    public FileSystem() {
+        try {
+            if (! Files.isDirectory(root)) {
+                Files.createDirectory(root);
+            }
+        } catch (IOException e) {
+            throw new UncheckedIOException(e);
+        }
+    }
 }
